4d02304346754a89518bd9adb866f9df2f8e86aa
AshishLawaju <ashish.lawaju74@gmail.com>
2024-04-03 07:50:49 +0545

    router

--- routes/blog.routes.ts
+++ routes/blog.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
-import {blogCreate, blogUpdate, blogDelete}from "../controllers/blog.controller";
+import {
+  blogCreate,
+  blogUpdate,
+  blogDelete,
+  likeBlog,
+  removeLikeBlog,
+} from "../controllers/blog.controller";
+import {
+  adminScope,
+  tokenExtractor,
+  verifyToken,
+} from "../middleware/tokenAuth";
 
-const router = Router()
+const router = Router();
 
-router.route("/createblog").post(blogCreate);
-router.route("/:id/update").post(blogUpdate);
-router.route("/:id/delete").delete(blogDelete);
-export default router
+router.route("/createblog").post(tokenExtractor, verifyToken, blogCreate);
+router.route("/:id/update").post(tokenExtractor, verifyToken, blogUpdate);
+router.route("/:id/delete").delete(tokenExtractor, adminScope, blogDelete);
+router.route("/like/:blogId").put(tokenExtractor, verifyToken, likeBlog);
+router.route("/removelike/:blogId").put(tokenExtractor, verifyToken, removeLikeBlog);
+export default router;
